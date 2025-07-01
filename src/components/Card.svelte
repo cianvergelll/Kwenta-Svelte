394b7a2aff34103da070a9ed5152e14620b03d13
Ian <cianvergelll@gmail.com>
2025-07-01 14:11:10 +0800

    Card Component

---
+++ src/components/Card.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { variant = 'primary', className = '' } = $props();
+
+	let classes = {
+		primary: 'rounded-xl bg-gradient-to-r from-green-600 to-green-800 shadow-xl',
+		secondary: 'flex-col items-center justify-center overflow-hidden rounded-xl bg-white shadow-xl'
+	};
+</script>
+
+<!-- svelte-ignore slot_element_deprecated -->
+<div class={`${classes[variant]} ${className}`}>
+	<slot />
+</div>
