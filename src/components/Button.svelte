b6264fe48bb4476b20cedc19cb5b10b72b2efccf
Ian <cianvergelll@gmail.com>
2025-07-01 11:13:49 +0800

    button reusable component

---
+++ src/components/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+	let {
+		ariaLabel = '',
+		type = 'button',
+		className = '',
+		variant = 'primary',
+		action = () => {}
+	} = $props();
+
+	let classes = {
+		primary: 'bg-blue-500 hover:bg-blue-600 text-white',
+		secondary: 'bg-gray-500 hover:bg-gray-600 text-white'
+	};
+</script>
+
+<button onclick={action} class={className} aria-label={ariaLabel} data-variant={variant}> </button>
